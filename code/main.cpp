f1b8ae73624bb1e423f1547bebc000c6b034d509
ÁRgB <angel.rodriguez@udit.es>
2026-04-14 09:42:14 +0200

    Added the StaticFileServer.

--- code/main.cpp
+++ code/main.cpp
@@ -6,21 +6,21 @@
 #include <iostream>
 #include <NetworkSetup.hpp>
 #include "SignalHandler.hpp"
-#include "HelloWorldRequestHandler.hpp"
+#include "StaticFileServer.hpp"
 
 using namespace argb;
 
-int main ()
+int main (int , const char * [])
 {
     try
     {
         NetworkSetup network_setup;
         HttpServer   server;
         Port         port{ 80 };
-        
-        HelloWorldRequestHandler hello_world_handler;
 
-        server.set_default_handler (&hello_world_handler);
+        StaticFileServer static_file_server("../../examples/static-website");
+
+        server.register_handler_factory ("/", static_file_server);
 
         std::cout << "Running HTTP server on port " << static_cast<uint16_t>(port) << "..." << std::endl;
 
